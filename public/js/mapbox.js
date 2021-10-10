8b21ca1942d2df38f6cb3e74012b7d209ce76bf0
Conal Walsh <conalwalsh@conals-mbp-2.home>
2021-10-10 18:03:10 +0100

    Completed mapbox section

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -8,5 +8,38 @@ mapboxgl.accessToken =
 
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
+  style: 'mapbox://styles/conal84/ckulg9fhr864g17nv01833zvh',
+  scrollZoom: false,
+  // center: [-118.113491, 34.111745],
+  // zoom: 6,
+  // interactive: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add Popup
+  new mapboxgl.Popup({ offset: 30 })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extends map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: { top: 200, bottom: 150, left: 100, right: 100 },
 });
