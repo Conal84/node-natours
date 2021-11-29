8241f6abd98b214f7683438f7073cf57ba002f1b
Conal Walsh <conal.walsh1@gmail.com>
2021-11-29 19:49:47 +0000

    Implemented stripe webhooks

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 
 const app = express();
 
@@ -87,6 +88,13 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour',
 });
 app.use('/api', limiter); // use the limiter middleware function on routes with /api
+
+// Stripe webhook checkout needs to be before body parser so response is not in JSON format
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, Needed for populating req.body
 app.use(express.json({ limit: '10kb' }));
